3b8fa7c294466d9518a747c0176327763e361609
Yousef Eltobji <150874709+YousefEltobji@users.noreply.github.com>
2024-06-19 00:48:35 -0400

    Updates

--- src/MyProjects.jsx
+++ src/MyProjects.jsx
@@ -3,7 +3,7 @@ import './MyProjects.css';
 import { FaGithub } from "react-icons/fa";
 import { MdConstruction } from "react-icons/md";
 import { FaArrowTrendUp } from "react-icons/fa6";
-
+import RESBuilderHomepageImg from "./images/RESBuilder-Homepage.png";
 
 const MyProjects = () => {
 
@@ -16,8 +16,8 @@ const MyProjects = () => {
                 </div>
                 <div className="project-section">
                     <div className="project-card">
-                        <span id="projectlogo"><MdConstruction size={150}/></span>
-                        <h3>RESBuilder (Coming soon)</h3>
+                        <img id="projectlogo" src={RESBuilderHomepageImg} alt="RESBuilder Homepage Image"/>
+                        <h3>RESBuilder</h3>
                         <p>RESBuilder is a dynamic web application built using ReactJS that allows users to upload and receive tailored feedback on their resumes, enhancing job application success.</p>
                         <p>Technologies used for this project include:</p>
                         <ul>
@@ -25,6 +25,7 @@ const MyProjects = () => {
                             <li>JavaScript</li>
                             <li>CSS</li>
                             <li>OpenAI API</li>
+                            <li>Express.JS</li>
                         </ul>
                         <a><button className="project-button">View Project <span id="buttonlogo"><FaArrowTrendUp size={22}/></span></button></a>
                     </div>
